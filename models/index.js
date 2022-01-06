81cba099e68c472673af6f57a2186bc96915a7c3
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2022-01-05 20:12:42 -0500

    seeded and routes data worked on

--- models/index.js
+++ models/index.js
@@ -5,13 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsTo(Tags, {
+  foreignKey: "productTag_id",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Product.belongsTo(Products, {
+  foreignKey: "productTag_id",
+});
 module.exports = {
   Product,
   Category,
