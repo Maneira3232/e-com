71b68dd5f6640ff9e39eb6f1d76b2e34a2d70d20
Maneira3232 <93150997+Maneira3232@users.noreply.github.com>
2022-01-10 10:46:44 -0500

    created a new tag in product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     ]
   })
   .then(product => res.json(product))
-})
+});
 
 // get one product
 router.get('/:id', (req, res) => {
@@ -38,14 +38,16 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
+  const body = req.body;
+  
+  try{
+    const newProduct = await Post.create({
       product_name: "Basketball",
       price: 200.00,
       stock: 3,
       tagIds: [1, 2, 3, 4]
-    }
-  */
+    });
+    
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
